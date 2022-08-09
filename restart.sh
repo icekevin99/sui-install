50d34b65e6d97208149cd27f6d8af3d8b9fa2ae9
iceone <48215451+icekevin99@users.noreply.github.com>
2022-08-09 22:06:05 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,10 +2,10 @@
 while [ 1 ] ; do
 sleep 3
     if [ $(ps -ef|grep /usr/local/bin/sui-node|grep -v grep|wc -l) -eq 0 ] ; then # 将exe_name替换成你想要监测的可执行程序名字
-        echo "[`date +%F\ %T`] exe_name is offline, try to restart..." >> ./blockchain/sui/monitor.log;
+        echo "[`date +%F\ %T`] exe_name is offline, try to restart..." >> /blockchain/sui/monitor.log;
         bash sui-full-node-install.sh upgrade &  # 将exe_name替换成你想要监测的可执行程序名字
     else
-        echo "[`date +%F\ %T`] exe_name is online..." >> ./blockchain/sui/monitor.log;
+        echo "[`date +%F\ %T`] exe_name is online..." >> /blockchain/sui/monitor.log;
     fi
-	  sleep 1m
+    sleep 1m
 done
